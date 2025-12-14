870b9d060ba448a9a40f84fcb26e7e04775eb416
Daniel Onoja <danielonoja246@gmail.com>
2025-12-14 13:01:47 +0100

    fix: remove font for complete offline usage

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,8 @@
 import type React from "react";
 import type { Metadata } from "next";
-import { Jost } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
 import { Toaster } from "@/components/ui/sonner";
 import "./globals.css";
-import { cn } from "@/lib/utils";
-
-const _jost = Jost({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "LanDropr - Local Network File Sharing",
@@ -23,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={cn(_jost.className, "dark")}>
+    <html lang="en" className="dark">
       <body className="antialiased">
         {children}
         <Toaster />
